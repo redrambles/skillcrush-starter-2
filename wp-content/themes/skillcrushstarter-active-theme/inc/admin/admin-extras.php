38b733aa0c945266aaab0c61ceb284437a4f6eea
Ann Cascarano <a.cascarano@gmail.com>
2016-01-18 20:08:32 -0500

    prepare to add js to admin / options

--- wp-content/themes/skillcrushstarter-active-theme/inc/admin/admin-extras.php
+++ wp-content/themes/skillcrushstarter-active-theme/inc/admin/admin-extras.php
@@ -12,5 +12,9 @@ function skillcrushstarter_load_admin_scripts( $page_hook ){
   }
   wp_register_style( 'skillcrushstarter-admin', get_template_directory_uri() . '/css/skillcrushstarter-admin.css', array(), '1.0.0', 'all' );
   wp_enqueue_style( 'skillcrushstarter-admin' );
+
+  wp_register_script( 'skillcrushstarter-admin-script', get_template_directory_uri().'/js/skillcrushstarter-admin.js', array('jquery'), '20160118', true);
+  wp_enqueue_script( 'skillcrushstarter-admin-script' );
+
 }
 add_action( 'admin_enqueue_scripts', 'skillcrushstarter_load_admin_scripts' );
