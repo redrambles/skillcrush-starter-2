0a36d72f89f8d909e68cb1e022a9c2e8de9f17fe
Ann Cascarano <a.cascarano@gmail.com>
2016-02-18 12:23:35 -0500

    a little code cleanup - not much to see here

--- wp-content/themes/skillcrushstarter-active-theme/index.php
+++ wp-content/themes/skillcrushstarter-active-theme/index.php
@@ -31,10 +31,7 @@ get_header(); ?>
 	</div>
 	
 	<?php get_sidebar(); ?>
-</section> <!-- I closed the section here instead of encompassing the navigation - which allowed me to use 'display:flex' on the element 
-to allow the both the content and the sidebar to have equal height - making the border follow all the way down. Problem: looks
-terrible when browser size is reduced - will have to look into proper use of flex - should probably fall back with a float and 
-build on responsiveness. -->
+</section> <!-- I closed the section here instead of encompassing the navigation - which allowed me to use 'display:flex' on the element to allow the both the content and the sidebar to have equal height - making the border follow all the way down. Problem: looks terrible when browser size is reduced - will have to look into proper use of flex - should probably fall back with a float and build on responsiveness. -->
 
 	<?php //if ( have_posts() ): ?>
 		<div id="navigation" class="container"> <!-- Here I added the 'span' as it was present in the other themes but not this one -->
