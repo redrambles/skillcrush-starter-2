9ea48bf2134cc73e9cb3e7aa6efb01198c8d897b
Ann Cascarano <a.cascarano@gmail.com>
2016-06-15 22:55:02 -0400

    more work on custom archive

--- wp-content/themes/skillcrushstarter-active-theme/templates/custom-archive.php
+++ wp-content/themes/skillcrushstarter-active-theme/templates/custom-archive.php
@@ -37,51 +37,55 @@ get_header(); ?>
 				
 				</div>
 				
-		  <?php
-		  	
-		  	$how_many_last_posts = intval(get_post_meta($post->ID, 'archive-num-posts', true));
-		  
-		  	if ( $how_many_last_posts > 200 || $how_many_last_posts < 2 ) $how_many_last_posts = 15;
+			<div class="latest-posts">
+			  <?php
+			  	
+			  	$how_many_last_posts = intval(get_post_meta($post->ID, 'archive-num-posts', true));
+			  
+			  	if ( $how_many_last_posts > 200 || $how_many_last_posts < 2 ) $how_many_last_posts = 15;
 
-				 $args = array (
+					 $args = array (
+							
+							'post_type' => 'post',
+							'posts_per_page' => $how_many_last_posts,
+							'order' => 'ASC',
+						); 
 						
-						'post_type' => 'post',
-						'posts_per_page' => $how_many_last_posts, // for some reason, this won't work.
-						'order' => 'ASC',
-					); 
-					
-					$my_query = new WP_Query($args);
+						$my_query = new WP_Query($args);
 
-		  	  if ( $my_query->have_posts() ) : ?>
+			  	  if ( $my_query->have_posts() ) : ?>
 
-		    	<h1 class="widget-title">Last <?php echo $how_many_last_posts ?> Posts <i class="fa fa-bullhorn" style="vertical-align: baseline;"></i></h1>&nbsp;
-		    	<div class="archives-latest-section">
-		    		<ol>
-		    	
-				    <?php while ( $my_query->have_posts()) : $my_query->the_post(); ?>
- 
-				      	<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
-				      
-				    <?php	endwhile; ?>
-		    
-		    	</ol>
-		    </div>
-		    
-		   	<?php wp_reset_postdata();
-		  		endif;
-		  	?>
+			    	<h1 class="how-many widget-title">Last <?php echo $how_many_last_posts ?> Posts <i class="fa fa-bullhorn" style="vertical-align: baseline;"></i></h1>&nbsp;
+			    	<div class="archives-latest-section">
+			    		<ol>
+			    	
+					    <?php while ( $my_query->have_posts()) : $my_query->the_post(); ?>
+	 
+					      	<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+					      
+					    <?php	endwhile; ?>
+			    
+			    	</ol>
+			    </div>
+			    
+			   	<?php wp_reset_postdata();
+			  		endif;
+			  	?>
+				</div>
 
+				<div class="custom-archive-footer">
 		  	  <h1 class="widget-title">Our Authors <i class="fa fa-user" style="vertical-align: baseline;"></i></h1>&nbsp;
-			  <div class="archives-authors-section">
-			    <ul>
-			      <?php wp_list_authors('exclude_admin=0&optioncount=1'); ?>
-			    </ul>
-			  </div>
+				  <div class="archives-authors-section">
+				    <ul>
+				      <?php wp_list_authors('exclude_admin=0&optioncount=1'); ?>
+				    </ul>
+				  </div>
 
-			  <h1 class="widget-title">By Month <i class="fa fa-calendar" style="vertical-align: baseline;"></i></h1>&nbsp;
-			  <div class="archives-by-month-section">
-			    <p><?php wp_get_archives('type=monthly&format=custom&after= |'); ?></p>
-			  </div>
+				  <h1 class="widget-title">By Month <i class="fa fa-calendar" style="vertical-align: baseline;"></i></h1>&nbsp;
+				  <div class="archives-by-month-section">
+				    <p><?php wp_get_archives('type=monthly&format=custom&after= |'); ?></p>
+				  </div>
+				</div>
 
 		</article>
 
