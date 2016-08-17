a7c275a364ca822ae7883659dd0c2b0b91a313b5
Ann Cascarano <a.cascarano@gmail.com>
2016-08-16 21:29:18 -0400

    update acf-theme-code plugin

--- wp-content/plugins/acf-theme-code/render/checkbox.php
+++ wp-content/plugins/acf-theme-code/render/checkbox.php
@@ -4,7 +4,8 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-$return_format = $this->settings['return_format'];
+$return_format = isset( $this->settings['return_format'] ) ? $this->settings['return_format'] : '';
+
 // echo htmlspecialchars('<h3>'.$return_format.'</h3>')."\n";
 
 // if return format is value or label (single array)
@@ -22,7 +23,7 @@ if($return_format == 'value' || $return_format == 'label') {
 } elseif($return_format == 'array') {
 
     // Return the code to output the value from a multi dimensional array
-    echo $this->indent . htmlspecialchars("<?php // ".$this->name." (".$return_format.")")."\n";
+    echo $this->indent . htmlspecialchars("<?php // ".$this->name." ( ".$return_format." )")."\n";
     echo $this->indent . htmlspecialchars("\$".$this->name."_array = ". $this->get_field_method . "( '" . $this->name . "' );")."\n";
     echo $this->indent . htmlspecialchars("if ( \$".$this->name."_array ):")."\n";
     echo $this->indent . htmlspecialchars("	foreach ( \$".$this->name."_array as \$".$this->name."_item ):")."\n";
--- wp-content/plugins/acf-theme-code/render/select.php
+++ wp-content/plugins/acf-theme-code/render/select.php
@@ -4,16 +4,17 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Check if single or multiple values can be stored
-$multiple_values = $this->settings['multiple'];
+// NOTE: value is a string
+$multiple_values = isset( $this->settings['multiple'] ) ? $this->settings['multiple'] : '';
 
 // Check for return format ACF 5.4 feature
-$return_format = $this->settings['return_format'];
+$return_format = isset( $this->settings['return_format'] ) ? $this->settings['return_format'] : '';
 
 // Dev - uncomment to debug this field
 //echo htmlspecialchars('<h3>'.$multiple_values.$return_format.'</h3>')."\n";
 
 // If single
-if($multiple_values == 0) {
+if($multiple_values == '0') {
 
 	// if this a single option returned as an array
 	if($return_format == 'array') {
@@ -35,7 +36,7 @@ if($multiple_values == 0) {
 }
 
 // If multiple
-if($multiple_values == 1) {
+if($multiple_values == '1' ) {
 
 	if($return_format == 'array') {
 
