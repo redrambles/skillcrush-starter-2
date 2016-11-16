df54397e07ec127643ed230a27faa21ca7926853
Ann Cascarano <a.cascarano@gmail.com>
2016-11-15 21:17:51 -0500

    worked on image captions

--- wp-content/themes/skillcrushstarter-active-theme/inc/extras.php
+++ wp-content/themes/skillcrushstarter-active-theme/inc/extras.php
@@ -72,3 +72,49 @@ function ajax_heart_add_love() {
     echo $love;
     die();
 }
+
+// Remove the extra 10px around image captions - credit Justin trader_cdladvanceblock
+// CAPTION remove the extra 10 px of horror
+add_filter( 'img_caption_shortcode', 'cleaner_caption', 10, 3 );
+
+function cleaner_caption( $output, $attr, $content ) {
+
+	/* We're not worried abut captions in feeds, so just return the output here. */
+	if ( is_feed() )
+		return $output;
+
+	/* Set up the default arguments. */
+	$defaults = array(
+		'id' => '',
+		'align' => 'alignnone',
+		'width' => '',
+		'caption' => ''
+	);
+
+	/* Merge the defaults with user input. */
+	$attr = shortcode_atts( $defaults, $attr );
+
+	/* If the width is less than 1 or there is no caption, return the content wrapped between the [caption]< tags. */
+	if ( 1 > $attr['width'] || empty( $attr['caption'] ) )
+		return $content;
+
+	/* Set up the attributes for the caption <div>. */
+	$attributes = ( !empty( $attr['id'] ) ? ' id="' . esc_attr( $attr['id'] ) . '"' : '' );
+	$attributes .= ' class="wp-caption ' . esc_attr( $attr['align'] ) . '"';
+	$attributes .= ' style="width: ' . esc_attr( $attr['width'] ) . 'px"';
+
+	/* Open the caption <div>. */
+	$output = '<div' . $attributes .'>';
+
+	/* Allow shortcodes for the content the caption was created for. */
+	$output .= do_shortcode( $content );
+
+	/* Append the caption text. */
+	$output .= '<p class="wp-caption-text">' . $attr['caption'] . '</p>';
+
+	/* Close the caption </div>. */
+	$output .= '</div>';
+
+	/* Return the formatted, clean caption. */
+	return $output;
+}
