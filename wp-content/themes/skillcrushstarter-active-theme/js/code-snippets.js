1fc7a123d8cc56fc42cf8b1b317ea6c03e64b87e
Ann Cascarano <a.cascarano@gmail.com>
2016-12-13 17:57:39 -0500

    removed build files

--- wp-content/themes/skillcrushstarter-active-theme/js/code-snippets.js
+++ wp-content/themes/skillcrushstarter-active-theme/js/code-snippets.js
@@ -1,3 +1,5 @@
+// To hide and then show gists that have been embedded
+
 jQuery(document).ready(function($) {
 // This solution is compliant with progressive enhancement principles - as the content will still show if JS is disabled.
 	$(".snippet").hide();
