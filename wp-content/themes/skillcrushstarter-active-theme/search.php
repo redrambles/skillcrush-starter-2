63fc998da8900e82d00a7308bfe030ecd54e870b
Ann Cascarano <a.cascarano@gmail.com>
2016-12-24 13:56:34 -0500

    improve search page styles

--- wp-content/themes/skillcrushstarter-active-theme/search.php
+++ wp-content/themes/skillcrushstarter-active-theme/search.php
@@ -34,14 +34,14 @@ get_header(); ?>
 	</div>
 	
 	<?php get_sidebar(); ?>
-
-	<?php if ( have_posts() ): ?>
-		<div id="navigation" class="container"> <!-- Here I added the 'span' as it was present in the other themes but not this one -->
-	        <div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
-	        <div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
-	    </div>
-	<?php endif; ?>
-
 </section>
+
+	<div id="navigation" class="navigation container">
+		<div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
+			<?php echo skillcrushstarter_custom_pagination(); ?>
+		<div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
+	</div>
+
+
 
 <?php get_footer(); ?>
