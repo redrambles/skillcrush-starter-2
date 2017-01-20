4b3a093e5e0ff9b422d54b79409a3360ad6a1027
Ann Cascarano <a.cascarano@gmail.com>
2017-01-19 20:44:05 -0500

    rest related posts plugin

---
+++ wp-content/plugins/rest-related-posts/js/related-posts.ajax.js
@@ -0,0 +1,45 @@
+/**
+* AJAX scripts
+**/
+
+(function($){
+  $('.get-related-posts').on( 'click', function(event) {
+    event.preventDefault();
+    var json_url = postdata.json_url; // this is defined in rest-related-posts.php
+    var post_id = postdata.post_id;
+    console.log(json_url);
+    console.log(post_id);
+    
+    // AJAX
+    $.ajax({
+      dataType: 'json',
+      url: json_url  
+    })
+    
+    .done(function(response){
+      console.log(response);
+      $.each(response, function(index, object){
+          var related_post_loop = '<aside class="related-post clear">' +
+                                  '<a href="'+ object.link + '">' + 
+                                  '<h1 class="related-post-title">' + object.title.rendered + '</h1>' +
+                                  '<div class-"related-excerpt">' + 
+                                  object.excerpt.rendered +
+                                  '<div>' +
+                                  '</a>' +
+                                  '</aside>';
+                                 
+          $('#related-posts').append(related_post_loop);
+      })
+    })
+    
+    .fail(function(){
+      console.log('DISASTER!!!!');
+    })
+    
+    .always(function(){
+      console.log('Complete');
+    });
+    
+  });
+  
+})(jQuery);
---
+++ wp-content/plugins/rest-related-posts/rest-related-posts.php
@@ -0,0 +1,88 @@
+<?php namespace Red_Plugins;
+/*
+* Plugin Name: Rest Related Posts
+* Description: Related Posts section using the WP-API and AJAX
+* Author: Ann Cascarano
+* Author URI: redrambles.com
+* Version: 1.0
+*/
+
+
+// TODO: This function below breaks the ajax for related posts - must find out what's going on
+// // Add extra fields to the JSON output
+//   function related_posts_register_fields() {
+//     // Add author name
+//     register_rest_field( 'post',
+//         'author_name',
+//         array(
+//             'get_callback'  => 'related_posts_get_author_name',
+//             'update_callback' => null,
+//             'schema'          => null,
+//         )
+//     );    
+//   }
+//   
+// function related_posts_get_author_name( $object, $field_name, $request ){
+//       return get_the_author_meta( $object['id'], $field_name, true );
+//     }
+//   add_action( 'rest_api_init', __NAMESPACE__ . '\\related_posts_register_fields' );
+
+
+add_filter( 'wp_enqueue_scripts', __NAMESPACE__ . '\\related_posts_scripts' );
+function related_posts_scripts( ) {
+  if ( is_single() && is_main_query() ) {
+    wp_enqueue_style( 'related-posts-styles', plugin_dir_url( __FILE__ ). 'css/related-posts-styles.css' );
+    wp_enqueue_script( 'related-posts-ajax-script', plugin_dir_url( __FILE__ ). 'js/related-posts.ajax.js', array('jquery'), '0,1', true);
+    
+    global $post;
+    $post_id = $post->ID;
+    
+    // Use this function to pass data from PHP to JS
+    wp_localize_script( 'related-posts-ajax-script', 'postdata', 
+      array (
+        'post_id' => $post_id,
+        'json_url' => rest_related_posts_json_query()
+      )
+    );
+  }
+}
+
+// Create REST API url 
+function rest_related_posts_json_query() {
+  $cats = get_the_category();
+  $cat_ids = array();
+  
+  foreach ( $cats as $category ) {
+    $cat_ids[] = $category->term_id;
+  }
+  
+  $url_args = array(
+    'categories' => implode( ",", $cat_ids),
+    'per_page' => 3
+  );
+  
+  $url = add_query_arg( $url_args, rest_url('wp/v2/posts?') );
+  return $url;
+}
+
+// Create the HTML output for the related post button - will be pulled in the related_posts display function
+function related_post_structure() {
+  $output = '<section id="related-posts" class="related-posts">';
+  $output .= '<a href="#" class="get-related-posts">Get related posts</a>';
+  //$output .= rest_related_posts_json_query(); // test the url
+  $output .= '<div class="ajax-loader"><img src="' . plugin_dir_url(__FILE__) .'css/spinner.svg"></div>';
+  $output .= '</section><!-- .related-posts -->';
+  return $output;
+}
+
+
+add_filter( 'the_content', __NAMESPACE__ . '\\related_posts' );
+
+// Display the related posts right after the post content 
+function related_posts( $content ) {
+  
+  if ( is_single() && is_main_query() ) {
+    return $content . related_post_structure();
+  }
+  return $content;
+}
